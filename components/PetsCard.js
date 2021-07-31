4b22d6e70a277085e8389b105819e63ad997e4f4
Daniel A-Sereboo <dsereboo@gmail.com>
2021-07-31 21:06:56 +0000

    Pet card component updated with proper image placement

--- components/PetsCard.js
+++ components/PetsCard.js
@@ -8,12 +8,12 @@ export default function PetsCard(){
         <View style={styles.cardContainer}>
             <Image style={styles.image} source={dog}/>
             <View style={styles.details}>
-                <Text style={styles.name}>Name</Text>
-                <Text>Animal</Text>
-                <Text>Age</Text>
+                <Text style={styles.title}>Name</Text>
+                <Text style={styles.detailText}>Dogo</Text>
+                <Text style={styles.detailText}>3 years old</Text>
                 <View style={styles.location}>
-                    <EvilIcons name="location" size={22} color="black" />
-                    <Text >Location</Text>
+                    <EvilIcons style="icon" name="location" size={24} color="#D3D3D3" />
+                    <Text numberOfLines={1} style={[styles.detailText, styles.locationText]}>No. 4 Lower Hill drive</Text>
                 </View>  
             </View>
         </View>
@@ -22,24 +22,30 @@ export default function PetsCard(){
 
 const styles=StyleSheet.create({
     image:{
-        width:170,
-        height: 180,
+        width:160,
+        height: 170,
         borderRadius:10,
         flex:0.6,
         zIndex:100,
-       
+        position:"absolute",
+        top:-15,
+        bottom:15,
+        right:100,
+        left:0,
     },
     cardContainer:{
         flexDirection:"row",
-        marginHorizontal:30,
+        marginHorizontal:28,
         backgroundColor:"#fff", 
         justifyContent:"flex-end" ,
         borderRadius:13,
+        marginVertical:30,   
     },
-    name:{
+    title:{
         color:"#4d4dff",
         fontSize:20,
         fontWeight:"bold",
+        marginTop:12,
     },
     profileText:{
         marginTop:10,
@@ -47,10 +53,27 @@ const styles=StyleSheet.create({
     details:{
         justifyContent:"center",
         alignItems:"flex-start",
-        marginLeft:10,
         flex:0.4,
+        marginHorizontal:15,
     },
     location:{
         flexDirection:"row",
+        justifyContent:"center",
+        alignItems:"center",
+        marginLeft:-8,
+        marginBottom:28,
+        marginTop:15,
+        flex:1,
     },
+    detailText:{
+        marginTop:5,
+        fontSize:18,
+        color:"#D3D3D3",   
+    },
+    icon:{
+       flex:0.1,
+    },
+    locationText:{
+        flex:0.9,
+    }
 })
