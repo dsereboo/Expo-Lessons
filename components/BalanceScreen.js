75f266bfbfc0bc5def0dffeb66599bb3598116cd
Daniel A-Sereboo <dsereboo@gmail.com>
2021-08-07 23:05:01 +0000

    Balance section of balance screen completed

--- components/BalanceScreen.js
+++ components/BalanceScreen.js
@@ -1,10 +1,38 @@
 import React from "react"
 import { StyleSheet, Text, View } from "react-native"
+import { Ionicons } from '@expo/vector-icons';
+import { Feather } from '@expo/vector-icons';
+import { FontAwesome5 } from '@expo/vector-icons';
 
 export default function BalaanceScreen(){
     return(
         <View style={styles.container}>
-            <View style={styles.balance}></View>
+            <View style={styles.balance}>
+                <View style={styles.header}>
+                    <Ionicons name="chevron-back" size={30} color="#fff" />
+                    <Feather name="bell" size={24} color="#bf80ff" />
+                </View>
+                <View style={styles.details}>
+                    <Text style={styles.title}>Your Balance</Text>
+                    <Text style={styles.amount}>$547,000.00</Text>
+                </View>
+                <View style={styles.cardRow}>
+                    <View style={styles.expense}>
+                        <View>
+                            <FontAwesome5 name="money-bill-wave" size={24} color="black" />
+                        </View>
+                        <View><Text>$5000</Text></View>
+                        <View><Text>Expense</Text></View>
+                    </View> 
+                    <View style={styles.goals}>
+                    <View>
+                            <FontAwesome5 name="money-bill-wave" size={24} color="black" />
+                        </View>
+                        <View><Text>$5000</Text></View>
+                        <View><Text>Expense</Text></View>
+                    </View>
+                </View>
+            </View>
             <View style={styles.transactions}></View>
 
         </View>
@@ -13,13 +41,60 @@ export default function BalaanceScreen(){
 
 const styles= StyleSheet.create({
     container:{
-        flex:1,
+        flex:10,
+        justifyContent:"center",
+        alignItems:"stretch"
     },
     balance:{
-        flex:0.5,
-        backgroundColor:"#0000ff",
+        flex:5,
+        backgroundColor:"#4d4dff",
+        
     },
     transactions:{
-        flex:0.5,
+        flex:4,
+        
     },
+    header:{
+        flex:1.8,
+        flexDirection:"row",
+        justifyContent:"space-between",
+        alignItems:"center",
+        // marginTop:40,
+        marginHorizontal:20,
+    },
+    details:{
+        flex:0.8,
+        justifyContent:"flex-start",
+        alignItems:"flex-start",
+        marginHorizontal:30,
+        marginBottom:20,
+    },
+    cardRow:{
+        flex:3,
+        flexDirection:"row",
+        justifyContent:"space-evenly",
+        marginVertical:30,
+    },
+    title:{
+        fontSize:24,
+        color:"#bf80ff"
+    },
+    amount:{
+        fontSize:32,
+        color:"#fff",
+    },
+    expense:{
+        flex:0.4,
+        backgroundColor:"#89CFF0",
+        borderRadius:10,
+        justifyContent:"center",
+        alignItems:"center"
+    },
+    goals:{
+        flex:0.4,
+        backgroundColor:"#ffdd99",
+        borderRadius:10,
+        justifyContent:"center",
+        alignItems:"center"
+    }
 })
