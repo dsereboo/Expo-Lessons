d628f8ece62a72fbbf2796d73d4f6bd306c5c141
Daniel A-Sereboo <dsereboo@gmail.com>
2021-08-14 22:34:22 +0000

    Skeleton layout for homepage complete

--- App.js
+++ App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import AddToCartScreen from './components/AddToCartScreen';
 import BalanceScreen from './components/BalanceScreen';
+import MintsHomepage from './components/MintsHomepage';
 import OnlineShoppingScreen from "./components/OnlineShoppingScreen"
 import PaymentSuccesfullScreen from './components/PaymentSuccesfullScreen';
 
 export default function App() {
   return(
-    <BalanceScreen/>
+    <MintsHomepage/>
   )
 }
 
--- components/MintsHomepage.js
+++ components/MintsHomepage.js
@@ -1,10 +1,55 @@
 import React from "react"
-import { View } from "react-native"
+import { StyleSheet, Text, TextInput, View } from "react-native"
 
 export default function MintsHomepage (){
     return(
-        <View>
-            
+        <View style={styles.container}>
+            <View>
+                <Text>Header row</Text>
+            </View>
+            <View>
+                <Text>Welcome Back</Text>
+                <Text>Creative Mints</Text>
+            </View>
+            <View>
+                <TextInput/>
+            </View>
+            <View>
+                <Text>Action cards</Text>
+            </View>
+            <View>
+                <Text>Choose a category</Text>
+            </View>
+            <View style={styles.categoryContainer}>
+                <View style={styles.logoContainer1}>
+                    <Text>Branch services</Text>
+                </View>
+                <View style={styles.logoContainer2}>
+                    <Text>Make a payment</Text>
+                </View>
+
+            </View>
         </View>
     )
 }
+
+const styles=StyleSheet.create({
+    container:{},
+    topHeader:{},
+    searchContainer:{},
+    categoryContainer:{
+        flexDirection:"row",
+    },
+    logoContainer1:{
+        backgroundColor:"#00b33c",
+        padding:12,
+        borderRadius:50,
+        marginRight:10,
+    },
+    logoContainer2:{
+        backgroundColor:"#00b33c",
+        padding:12,
+        borderRadius:50,
+        marginRight:10,
+    },
+})
