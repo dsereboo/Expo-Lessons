fa64c56a8bf71a75fa96f0decce3ca7ea56ccd97
Daniel A-Sereboo <dsereboo@gmail.com>
2021-08-15 13:57:27 +0000

    Top header for mints homepage complete

--- App.js
+++ App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import AddToCartScreen from './components/AddToCartScreen';
 import BalanceScreen from './components/BalanceScreen';
-import MintsHomepage from './components/MintsHomepage';
 import OnlineShoppingScreen from "./components/OnlineShoppingScreen"
 import PaymentSuccesfullScreen from './components/PaymentSuccesfullScreen';
+import MintsHomePage from "./components/MintsHomepage"
 
 export default function App() {
   return(
-    <MintsHomepage/>
+    <MintsHomePage/>
   )
 }
 
--- components/MintsHomepage.js
+++ components/MintsHomepage.js
@@ -1,20 +1,29 @@
 import React from "react"
-import { StyleSheet, Text, TextInput, View } from "react-native"
+import { Image, StyleSheet, Text, TextInput, View } from "react-native"
 import { FontAwesome } from '@expo/vector-icons';
 import { AntDesign } from '@expo/vector-icons';
+import { Feather } from '@expo/vector-icons';
+import { FontAwesome5 } from '@expo/vector-icons';
 
 export default function MintsHomepage (){
     return (
       <View style={styles.container}>
-        <View>
-          <Text>Header row</Text>
+        <View style={styles.topHeader}>
+          <View style={styles.optionsContainer}>
+            <FontAwesome5 name="grip-lines" size={24} color="#00308F" />
+          </View>
+          <View style={styles.imageContainer}>
+            <FontAwesome name="bell-o" size={22} color="#00308F" />
+            <Image style={styles.userImage} source={require("../assets/image.jpg")}/>
+          </View>
         </View>
         <View style={styles.greetingContainer}>
           <Text style={styles.greeting}>Welcome Back</Text>
           <Text style={styles.name}>Creative Mints</Text>
         </View>
-        <View>
-          <TextInput placeholder="Search" />
+        <View style={styles.searchContainer}>
+          <Feather name="search" size={20} color="black" />
+          <TextInput style placeholder="Search" />
         </View>
         <View>
           <Text>Action cards</Text>
@@ -27,15 +36,18 @@ export default function MintsHomepage (){
             <View style={styles.logoContainer1}>
               <FontAwesome name="bank" size={18} color="#fff" />
             </View>
-            <View>
+            <View style={styles.branchText}>
                 <Text>Branch services</Text>
             </View>
           </View>
-          <View>
+          <View style={styles.branch}>
             <View style={styles.logoContainer2}>
-                <AntDesign name="creditcard" size={24} color="#fff" />
+                <AntDesign name="creditcard" size={18} color="#fff" />
             </View>
-            <Text>Make a pyament</Text>
+            <View>
+                <Text>Make a payment</Text>
+            </View>
+            
           </View>
           
         </View>
@@ -47,18 +59,30 @@ const styles=StyleSheet.create({
     container:{
         flex:10,
         marginHorizontal:20,
+        marginTop:40,
     },
-    topHeader:{},
+    topHeader:{
+      flexDirection:"row",
+      flex:0.5,
+      alignItems:"center",
+      justifyContent:"space-between"
+    },
     greetingContainer:{
-        flex:1,
+        flex:2,
     },
-    searchContainer:{},
+    searchContainer:{
+        flex:0.5,
+        flexDirection:"row",
+        backgroundColor:"#F0F8FF",
+        alignItems:"center",
+        borderRadius:20,
+        justifyContent:"flex-start"
+    },
     categoryContainer:{
         flexDirection:"row",
         justifyContent:"space-between",
         flex:1,
-        alignItems:"center"
-        
+        alignItems:"center"  
     },
     logoContainer1:{
         backgroundColor:"#66e0ff",
@@ -72,6 +96,21 @@ const styles=StyleSheet.create({
         borderRadius:50,
         marginRight:10,
     },
+    optionsContainer:{
+      backgroundColor:"#F0F8FF",
+      padding:10,
+      borderRadius:50,
+    },
+    userImage:{
+      width:45,
+      height:45, 
+      borderRadius:50,
+      marginLeft:20,
+    },
+    imageContainer:{
+      flexDirection:"row",
+      alignItems:"center"
+    },
     categoryHeader:{
         fontSize:26,
         fontWeight:"bold",
@@ -84,6 +123,9 @@ const styles=StyleSheet.create({
         borderColor:"#f8f8f8",
         borderRadius:10,
         padding:5,
+    },
+    branchText:{
+       flexDirection:"column"
     },
     greeting:{
         fontSize:24,
